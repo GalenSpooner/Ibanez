dcc3ee423c4c7009b6652826bb8314a5560dfe78
GalenSpooner <gspooner@cusd.me>
2025-08-04 15:47:39 -0700

    added controls

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,13 +4,22 @@
 
 package frc.robot;
 
+import frc.robot.Constants.LEDConstants;
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Climb;
+import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.EndEffector;
+import frc.robot.subsystems.Superstructure;
+import frc.robot.subsystems.Climb.ClimbState;
+import frc.robot.subsystems.EndEffector.EndEffectorState;
+import frc.robot.subsystems.Superstructure.SuperstructureState;
+
+import com.ctre.phoenix6.hardware.CANdle;
+
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -20,35 +29,65 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  public CANdle leds = new CANdle(LEDConstants.kCandleID);
+  public Elevator elevator = new Elevator();
+  public Arm arm = new Arm(leds);
+  public EndEffector endEffector = new EndEffector(leds);
+  public Climb climb = new Climb();
+  public Superstructure superstructure = new Superstructure(elevator, arm);
+  
+  
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
+  
+  private final CommandXboxController controller =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
+
+  private final CommandJoystick guitar = 
+      new CommandJoystick(1);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    
     // Configure the trigger bindings
     configureBindings();
   }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+      controller.a().onTrue(superstructure.requestSuperstructureState(SuperstructureState.L1))
+      .onFalse(superstructure.requestSuperstructureState(SuperstructureState.STOW));
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+      controller.b().onTrue(superstructure.requestSuperstructureState(SuperstructureState.L2))
+      .onFalse(superstructure.requestSuperstructureState(SuperstructureState.STOW));
+
+      controller.x().onTrue(superstructure.requestSuperstructureState(SuperstructureState.L3))
+      .onFalse(superstructure.requestSuperstructureState(SuperstructureState.STOW));
+
+      controller.y().onTrue(superstructure.requestSuperstructureState(SuperstructureState.L4))
+      .onFalse(superstructure.requestSuperstructureState(SuperstructureState.STOW));
+
+      controller.povUp().onTrue(superstructure.requestSuperstructureState(SuperstructureState.BARGE))
+      .onFalse(superstructure.requestSuperstructureState(SuperstructureState.STOW));
+
+      controller.povLeft().onTrue(superstructure.requestSuperstructureState(SuperstructureState.ALGLOW)
+      .alongWith(endEffector.requestEndEffectorState(EndEffectorState.INTAKE)))
+      .onFalse(superstructure.requestSuperstructureState(SuperstructureState.STOW));
+
+      controller.povRight().onTrue(superstructure.requestSuperstructureState(SuperstructureState.ALGHIGH)
+      .alongWith(endEffector.requestEndEffectorState(EndEffectorState.INTAKE)))
+      .onFalse(superstructure.requestSuperstructureState(SuperstructureState.STOW));
+
+      controller.leftTrigger().onTrue(superstructure.requestSuperstructureState(SuperstructureState.INTAKE)
+      .alongWith(endEffector.requestEndEffectorState(EndEffectorState.INTAKE)))
+      .onFalse(superstructure.requestSuperstructureState(SuperstructureState.STOW));
+
+      controller.rightTrigger().onTrue(endEffector.requestEndEffectorState(EndEffectorState.OUTTAKE))
+      .onFalse(endEffector.requestEndEffectorState(EndEffectorState.HOLD));
+
+
+
+      guitar.button(0).onTrue(climb.setClimbState(ClimbState.DEPLOY));
+
+      guitar.button(1).onTrue(climb.setClimbState(ClimbState.STOW));
   }
 
   /**
@@ -57,7 +96,6 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return null;
   }
 }
